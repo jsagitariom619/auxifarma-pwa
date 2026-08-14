0dc62a3e240aff28a0bbc0ed77d34b4d192e4ad4
jsagitariom619 <jsagitariom619@gmail.com>
2026-08-13 22:18:12 -0400

    fix: refresh offline cache for v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='auxifarma-v2';
+const CACHE='auxifarma-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./src/css/app.css','./src/js/app.js','./src/data/categories.js','./icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
